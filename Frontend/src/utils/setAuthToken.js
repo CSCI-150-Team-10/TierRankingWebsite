6747837f719b2b23e8302ca9de8e69de0e09b49f
James Hernandez <hernandezjn97@gmail.com>
2019-12-10 02:50:46 -0800

    Update setAuthToken.js
    updated setAuth token.js

--- Frontend/src/utils/setAuthToken.js
+++ Frontend/src/utils/setAuthToken.js
@@ -5,6 +5,7 @@ const setAuthToken = token => {
   //like in postman
   if (token) {
     //checks the token from local storage
+    //we will be using axios because we will be making requests to the x-auth headers and set them to token
     axios.defaults.headers.common["x-auth-token"] = token; //set the headers
   } else {
     delete axios.defaults.headers.common["x-auth-token"]; //if the token is not there delete from the global headers
